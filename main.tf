24bb76aec9a0118574b6854eb508c7083516e609
Dermot reynolds <dermotreynolds@Dermots-MacBook-Pro.local>
2018-08-01 13:08:48 +0200

    first commit

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_key_vault" "nft_key_vault" {
 
   access_policy {
     tenant_id = "${data.azurerm_client_config.current.tenant_id}"
-    object_id = "5014aa70-8762-4899-b586-3e548732a78a"
+    object_id = "${data.azurerm_client_config.current.service_principal_object_id}"
 
     key_permissions = [
       "create",
